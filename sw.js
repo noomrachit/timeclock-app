bbb24ff550b7168cbc5eb31ee13265d281d4081a
SoLARLIT <49103973+noomrachit@users.noreply.github.com>
2026-09-07 02:37:51 +0300

    Add iOS PWA files

--- sw.js
+++ sw.js
@@ -1,5 +1,27 @@
-const CACHE="timeclock-v1";
-const FILES=["./","./index.html","./manifest.json","./icon.svg"];
-self.addEventListener("install", e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))); self.skipWaiting(); });
-self.addEventListener("activate", e=>{ e.waitUntil(self.clients.claim()); });
-self.addEventListener("fetch", e=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
+const CACHE = "timeclock-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
